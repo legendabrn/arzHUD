b0c0203b8658a7fbf5a60d9d7b4a84ecdf502f05
legendabrn <84228488+legendabrn@users.noreply.github.com>
2021-09-03 12:30:47 +0700

    Update net.h

--- net.h
+++ net.h
@@ -21,30 +21,6 @@ namespace net
 		return urmem::call_function<urmem::calling_convention::thiscall, bool>(Addresses::FUNC_RAKSERVER__SEND, Addresses::PTR_RAKSERVER, bitStream, priority, reliability, orderingChannel, playerId, broadcast);
 	}
 
-	class thiscall_hooks 
-	{
-	public:
-		static int THISCALL hook_connect(void* dis, int a1)
-		{
-			RakNet::BitStream arz;
-			arz.Write(((uint8_t)(220u)));
-			arz.Write(((uint8_t)(6u)));
-			arz.Write(((uint8_t)(2u)));
-			Send(&arz, 1, 9, '\0', GetPlayerIDFromIndex(a1), false);
-			arz.Reset();
-			arz.Write(((uint8_t)(220u)));
-			arz.Write(((uint8_t)(10u)));
-			arz.Write(((uint8_t)(2u)));
-			Send(&arz, 1, 9, '\0', GetPlayerIDFromIndex(a1), false);
-			arz.Reset();
-			arz.Write(((uint8_t)(220u)));
-			arz.Write(((uint8_t)(11u)));
-			arz.Write(((uint8_t)(2u)));
-			Send(&arz, 1, 9, '\0', GetPlayerIDFromIndex(a1), false);
-			return hkConnect->call<urmem::calling_convention::thiscall, int>(dis, a1);
-		}
-	};
-
 	static urmem::address_t GetRakServerInterface() 
 	{
 		const auto rakserver = hkRakNet->call<urmem::calling_convention::cdeclcall, urmem::address_t>();
@@ -56,16 +32,12 @@ namespace net
 	static bool init(void* addr_server)
 	{
 		urmem::sig_scanner scan;
-		urmem::address_t addr_raknet, addr_connect;
+		urmem::address_t addr_raknet;
 
 		scan.init((urmem::address_t)addr_server);
 
 		scan.find(pattern, mask, addr_raknet);
-		scan.find(pattern_connect, mask_connect, addr_connect);
-
 		hkRakNet = std::make_shared<urmem::hook>(addr_raknet - 7, urmem::get_func_addr(&GetRakServerInterface));
-		//hkConnect = std::make_shared<urmem::hook>(addr_connect, urmem::get_func_addr(&thiscall_hooks::hook_connect));
-
 		return true;
 	}
 };
